c9536d43f8368298f9a60ff9ad0f2e1479dc7408
KOOK050405 <KOOK050405@gmail.com>
2025-07-27 23:56:58 +0800

    pull hello.java

--- src/main/java/aaa/hello.java
+++ src/main/java/aaa/hello.java
@@ -2,7 +2,7 @@ package aaa;
 class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("push_test"
-        );
+        System.out.println("push_test" );
+         System.out.println("pill_test" );
     }
 }
